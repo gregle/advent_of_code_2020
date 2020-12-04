f93ddf018dfb3ffddd9a7ac26ba4af8707412ef0
Gregle <jusman98@gmail.com>
2020-12-03 19:40:43 -0800

    this was a fun one to think through and turned out to be pretty straight forward once you can conceptualize it

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,18 +1,58 @@
-# import os
-# import re
-# PATH = os.path.join(os.getcwd(), 'day_3\\input.txt')
+import os
+PATH = os.path.join(os.getcwd(), 'day_3\\input.txt')
 
-# class Day3_p1:        
-#     def doTheThing(self):
-#         with open(PATH, 'r') as f:
+class Day3_p1:
+    def doTheThing(self):
+        playerPosition = 0
+        trees = 0
+        with open(PATH, 'r') as f:
+            # read line one because we need to know the line length
+            lineLength = len(f.readline().rstrip())
+            # then iterate through the rest of the lines (moving down the slope one step at a time)
+            for line in f:
+                # advance the player to the right three spaces
+                # then mod the player position to loop back to the start of the line
+                # (ie. starting on the next tile to the right)
+                playerPosition = (3 + playerPosition) % lineLength
+                # check for trees
+                if line[playerPosition] == '#':
+                    trees += 1
+        return trees
 
-# class Day3_p2:        
-#     def doTheThing(self):
-#         with open(PATH, 'r') as f:
 
-# if __name__ == "__main__":
-#     d3p1 = Day3_p1()
-#     print(d3p1.doTheThing())
+class Day3_p2:
+    def doTheThingTweakedFromPart1(self, rightRate, downRate):
+        playerPosition = 0
+        trees = 0
+        with open(PATH, 'r') as f:
+            lineLength = len(f.readline().rstrip())
+            for line in f:
 
-#     d3p2 = Day3_p2()
-#     print(d3p2.doTheThing())
+                # this time the right rate comes from the function call
+                playerPosition = (rightRate + playerPosition) % lineLength
+                # and we can skip extra lines per the downrate
+                # starting at 1 because of how numbers work
+                for downMovement in range(1, downRate):
+                    line = f.readline()
+
+                if line[playerPosition] == '#':
+                    trees += 1
+        return trees
+    
+    def doTheThing(self):
+        arrayOfSlopes = [1, 3, 5, 7]
+        result = 1
+        # do all the 1 down slopes
+        for slope in arrayOfSlopes:
+            result *= self.doTheThingTweakedFromPart1(slope, 1)
+        # then do the 2 down slope
+        result *= self.doTheThingTweakedFromPart1(1, 2)
+        return result
+        
+
+if __name__ == "__main__":
+    d3p1 = Day3_p1()
+    print(d3p1.doTheThing())
+
+    d3p2 = Day3_p2()
+    print(d3p2.doTheThing())
